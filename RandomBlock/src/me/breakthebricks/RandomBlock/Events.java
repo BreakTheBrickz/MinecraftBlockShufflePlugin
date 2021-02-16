10a3639745cf1901c7cec5aa817124f1f4204168
BreakTheBrickz <jan.spiegler@gmail.com>
2021-02-16 12:36:20 +0100

    ttr

--- RandomBlock/src/me/breakthebricks/RandomBlock/Events.java
+++ RandomBlock/src/me/breakthebricks/RandomBlock/Events.java
@@ -8,6 +8,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
@@ -99,6 +100,5 @@ public class Events implements Listener {
 		
 	}
 		
-	
-	
+		
 }
